f156ae1345a368811be896c22927859d01086ec3
guiwenhui <2356991580@qq.com>
2026-04-19 01:41:36 +0800

    默认头像

--- aitodo/src/components/Leaderboard.vue
+++ aitodo/src/components/Leaderboard.vue
@@ -33,7 +33,7 @@
             <div class="rank-number">0{{ item.rank }}</div>
 
             <div class="avatar-container">
-              <img :src="item.avatarUrl || `https://api.dicebear.com/7.x/notionists/svg?seed=${item.username}`"
+              <img :src="item.avatarUrl || '/vue/default-avatar.png'" class="avatar" />
                    class="avatar" />
               <div v-if="item.rank === 1" class="crown-icon">
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
